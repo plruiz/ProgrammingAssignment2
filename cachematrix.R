313881e6aca69c7b48fc98e5d09e2381994d90a9
plruiz <plruiz@espe.edu.ec>
2014-11-23 14:47:00 -0500

    Revert "ProgrammingAssignment2 / cachematrix.R"
    This reverts commit b2eab6eaefba73ac32e8e7df990eccb9928ae23c.

--- cachematrix.R
+++ cachematrix.R
@@ -1,49 +1,15 @@
-## References: Cache Mean Example given in the assignment description, discussion forums of the course.
+## Put comments here that give an overall description of what your
+## functions do
 
-## This is the prpgraam to compute the inverse of the matrix and cache the result so as to retreive 
-## from the cache whenever call to this function is made, thus saving the time for computing the inverse
-## everytime.
-
-## makeCacheMatrix()
-## Input - matrix x to be created
-## Output - list of functions (get, set, getInverse, setInverse)
-## get - Returns the data in the matrix x
-## set - Sets matrix x with the data passed as parameter
-## getinv - gets the inverse of the matrix x
-## setinv - sets the inverse of the matrix x through the function parameter/arguement
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+
 }
 
 
-## cacheSolve()
-## Input - x, the matrix whose inverse needs to be computed
-## Output - the inverse of the matrix x
-## Flow: Invokes the already computed mean through getinv()
-##       If it is NULL, the it gets the matrix using get(), then computes the inverse 
-##          and sets the inverse through setinv
-##       Else, it returns the cached value
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data)
-  x$setinv(inv)
-  inv
+        ## Return a matrix that is the inverse of 'x'
 }
